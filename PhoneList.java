b2720955868e2c09b205aefe6c7bb66717fa52f5
CynicalMrJones <wopjr1234@gmail.com>
2026-01-20 18:28:24 -0500

    MEMES

---
+++ PhoneList.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+public class PhoneList {
+    private ArrayList<Phone> phoneArray = new ArrayList<Phone>() ;
+
+    public ArrayList<Phone> getPhoneArray() {
+        return phoneArray;
+    }
+
+    public void addPhone(Phone phone){
+        phoneArray.add(phone);
+    }
+
+    public void deletePhone(Phone phone){
+        phoneArray.remove(phone);
+    }
+
+    public void printPhoneArray(){
+        for (int i = 0; i < phoneArray.toArray().length; i++) {
+            System.out.println(phoneArray.get(i).getPhoneString());
+        }
+    }
+}
